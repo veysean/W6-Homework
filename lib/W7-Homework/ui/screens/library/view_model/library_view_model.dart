bc8333e1c2f4b7159230606b91230f1fcb662e62
vesyean <veyseankem@gmail.com>
2026-03-06 08:24:06 +0700

    update view model

--- lib/W7-Homework/ui/screens/library/view_model/library_view_model.dart
+++ lib/W7-Homework/ui/screens/library/view_model/library_view_model.dart
@@ -1,22 +1,37 @@
 import 'package:flutter/material.dart';
-
 import '../../../../data/repositories/songs/song_repository.dart';
+import '../../../../data/repositories/songs/user_history_repository.dart';
 import '../../../../model/songs/song.dart';
 import '../../../states/player_state.dart';
 
 class LibraryViewModel extends ChangeNotifier {
   final SongRepository songRepository;
   final PlayerState playerState;
+  final UserHistoryRepository userHistoryRepository;
 
-  LibraryViewModel({required this.songRepository, required this.playerState});
+  LibraryViewModel({
+    required this.songRepository,
+    required this.playerState,
+    required this.userHistoryRepository,
+  });
 
   List<Song> _songs = [];
   List<Song> get songs => _songs;
+
+  List<Song> _recentSongs = [];
+  List<Song> get recentSongs => _recentSongs;
 
   Song? get currentSong => playerState.currentSong;
 
   void init() {
     _songs = songRepository.fetchSongs();
+
+    final recentIds = userHistoryRepository.fetchRecentSongIds();
+    _recentSongs = recentIds
+        .map((id) => songRepository.fetchSongById(id))
+        .whereType<Song>()
+        .toList();
+
     // Listen to player state changes
     playerState.addListener(() {
       notifyListeners();
