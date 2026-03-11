7511f21d2d457f261f752d4fd1076a5ebf12b4b8
vesyean <veyseankem@gmail.com>
2026-03-11 18:15:18 +0700

    test using both then and try-catch

--- lib/W8-Homework/test/song_repository_test.dart
+++ lib/W8-Homework/test/song_repository_test.dart
@@ -1,10 +1,62 @@
- 
-void main() async {
+import '../data/repositories/songs/song_repository_mock.dart';
+
+// // - Using async/await with try/catch.
+// void main() async {
+//   //   Instantiate the  song_repository_mock
+
+//   // Test both the success and the failure of the post request
+
+//   // Handle the Future using 2 ways  (2 tests)
+//   // - Using then() with .catchError().
+//   // - Using async/await with try/catch.
+
+//   // This code will display song title
+//   final repo = SongRepositoryMock();
+//   try {
+//     final song = await repo.fetchSongById('s1');
+//     print('Song title ${song?.title}');
+//   } catch (e) {
+//     print(e);
+//   }
+
+//   // This code will throw an error
+//   try {
+//     final song = await repo.fetchSongById('s10');
+//     print('Song title ${song?.title}');
+//   } catch (e) {
+//     print(e);
+//   }
+// }
+
+// - Using then() with .catchError().
+void main() {
   //   Instantiate the  song_repository_mock
- 
+
   // Test both the success and the failure of the post request
- 
+
   // Handle the Future using 2 ways  (2 tests)
   // - Using then() with .catchError().
   // - Using async/await with try/catch.
+
+  final repo = SongRepositoryMock();
+
+  // This code will display song title
+  repo
+      .fetchSongById('s1')
+      .then((song) {
+        print('Song title ${song?.title}');
+      })
+      .catchError((error) {
+        print(error);
+      });
+
+  // This code will throw an error
+  repo
+      .fetchSongById('s10')
+      .then((song) {
+        print('Song title ${song?.title}');
+      })
+      .catchError((error) {
+        print(error);
+      });
 }
